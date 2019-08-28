c0ac3ea1a6ad81bcf977a87a01d9ffd4371fe91d
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-08-28 16:41:46 +0900

    Remove unnecessary @types/cheerio

--- packages/node-web-scraper/src/index.ts
+++ packages/node-web-scraper/src/index.ts
@@ -1,10 +1,34 @@
+/// <reference types="cheerio" />
+
 import * as scrapeIt from 'scrape-it';
-import {
-  ScrapeOptions,
-  ScrapeOptionElement,
-  ScrapeOptionList,
-  ScrapeResult,
-} from 'scrape-it';
+
+interface ScrapeOptions {
+  [key: string]: string | ScrapeOptionList | ScrapeOptionElement;
+}
+
+interface ScrapeOptionElement {
+  selector?: string;
+  convert?: (value: any) => any;
+  how?: string | ((element: CheerioSelector) => any);
+  attr?: string;
+  trim?: boolean;
+  closest?: string;
+  eq?: number;
+  texteq?: number;
+}
+
+interface ScrapeOptionList {
+  listItem: string;
+  data?: ScrapeOptions;
+  convert?: (value: any) => any;
+}
+
+interface ScrapeResult<T> {
+  data: T,
+  $: Cheerio,
+  response: any,
+  body: string
+}
 
 interface ScraperConfig {
   target: string;
