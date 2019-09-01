5eb5729f23460b94812036f237dcdb6750ad81b8
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-09-02 00:15:47 +0900

    Export useful type definitions

--- packages/node-web-crawler/index.d.ts
+++ packages/node-web-crawler/index.d.ts
@@ -1,11 +1,19 @@
 import crawl from './dist/packages/node-web-crawler/lib/index';
 import {
+  ScrapeConfig,
+  ScrapeOptions,
+  ScrapeOptionElement,
+  ScrapeOptionList,
   CrawlConfig,
   CrawlLinkOptions,
 } from './dist/interfaces';
 
 export default crawl;
 export {
+  ScrapeConfig,
+  ScrapeOptions,
+  ScrapeOptionElement,
+  ScrapeOptionList,
   CrawlConfig,
   CrawlLinkOptions,
 };
--- packages/node-web-fetch/index.d.ts
+++ packages/node-web-fetch/index.d.ts
@@ -1,11 +1,19 @@
 import fetch from './dist/packages/node-web-fetch/lib/index';
 import {
   ScrapeConfig,
+  ScrapeOptions,
+  ScrapeOptionElement,
+  ScrapeOptionList,
   CrawlConfig,
+  CrawlLinkOptions,
 } from './dist/interfaces';
 
 export default fetch;
 export {
   ScrapeConfig,
+  ScrapeOptions,
+  ScrapeOptionElement,
+  ScrapeOptionList,
   CrawlConfig,
+  CrawlLinkOptions,
 }
