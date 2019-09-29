4074aa7f12178094d1e5acbb55a2381a4c79aa0a
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-09-30 03:00:03 +0900

    Remove unnecessary imports

--- packages/node-web-crawler/index.d.ts
+++ packages/node-web-crawler/index.d.ts
@@ -3,12 +3,6 @@ import {
   CrawlConfig,
   CrawlLinkOptions,
 } from './dist/interfaces';
-import {
-  ScrapeOptions,
-  ScrapeOptionElement,
-  ScrapeOptionList,
-  ScrapeResult,
-} from 'scrape-it';
 
 export default crawl;
 
